aec8fb8b6d31f804ff3b4ff5203f36d879f83bd3
JorgeFullScout777 <jorgefullscout@gmail.com>
2023-11-22 13:40:43 -0600

    commit test

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
