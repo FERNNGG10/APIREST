d8ef3bc82b40754442911575a51b547f4417f3bb
JorgeFullScout777 <jorgefullscout@gmail.com>
2023-11-24 13:07:54 -0600

    prueba

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirstController extends Controller
+{
+    public function insert(){
+
+    }
+
+    public function delete(){
+
+    }
+
+    public function read(){
+        
+    }
+
+    public function update(){
+
+    }
+}
